d8bb7ecab567014e7deedfadaedf60541448591a
Christine Ooi <christine.ooi@gmail.com>
2018-04-19 13:07:49 -0400

    Modified client script main.js to fetch for new images every 5 seconds

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const app = express();
 const path = './public/uploads';
 const items = [];
 
+
 app.use(express.static('public'));
+app.use(express.json());
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {  
-    
     fs.readdir(path, function(err, items) {
         console.log(items);    
         res.render('indexget.pug',{title: 'KenzieGram', arrayofimages: items});
@@ -21,25 +22,26 @@ app.get('/', function (req, res) {
 })
 
 app.post('/latest', function (req, res, next) {
-    console.log("req.body: " + req.body);
-    console.log("req.body.after" + req.body.after);
+    console.log("In server script, timestamp being passed in - req.body.after: " + req.body.after);
+    const latestImages = [];
     const after = req.body.after;
-    const newImgsObj = [{timestamp: 0}];
-    const maxTimestamp = 0;
+    let maxTimestamp = 0;
     fs.readdir(path, function(err, items) {
         for (let i=0; i<items.length; i++){
-            let modified = fs.statSync(items[i]).mtimeMs;
+            let modified = fs.statSync(path + '/' + items[i]).mtimeMs;
             console.log("modified: " + modified);
             if (modified > after){
-                newImgsObj.push({images: items[i]});
-                maxTimestamp = modified > maxTimestamp ? modifed : maxTimestamp;
+                latestImages.push(items[i]);
+                maxTimestamp = modified > maxTimestamp ? modified : maxTimestamp;
             }
         }
-        if (maxTimestamp !== 0) {
-            newImgsObj.timestamp = maxTimestamp;
-        }
+        
+        console.log("Sending latestImages back to client", latestImages);
+        console.log("latestImages.length", latestImages.length);
+        console.log("Sending maxTimestamp back to client", maxTimestamp);
+        res.send({images: latestImages, timestamp: maxTimestamp});
     });
-    res.send(newImgsObg);
+    
 })
 
 app.post('/upload', upload.single('myFile'), function (req, res, next) {
--- public/main.js
+++ public/main.js
@@ -1,32 +1,46 @@
-const fs = require('fs');
-const imagecontainer = document.getElementById("imagecontainer");
+const imageContainerDiv = document.getElementById("imagecontainer");
+let latestImages = [{timestamp: 0}];
+let maxTimestamp = Date.now();
 
-//Get max timestamp of any images loaded so far
-// function getMaxTimestamp(){
-
-//     return 
-// }
+function appendImage(anImg) {
+    latestImages.push(anImg);
+    imageContainerDiv.innerHTML =
+      `<div><img src="uploads/${anImg}"></div>` + imageContainerDiv.innerHTML;
+}
 
 function fetchImages() {
 
-    // const postRequestOptions = {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({after: 1111}),
-    // }
+    console.log("In FetchImages: maxTimestamp", maxTimestamp);
 
-    // fetch('/latest', postRequestOptions)
-    // .then(response => response.json())
-    // .then(data => {      
-    //     console.log(data);
-    //     const json = JSON.stringify(data);
-    //     // displayScores(JSON.parse(json));
-    // })
-    // .catch(error => {
-    //     console.log("An error has occurred when attempting to Fetch:", error)
-    // })
+    const postRequestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({after: maxTimestamp}),
+    }
+
+    fetch('/latest', postRequestOptions)
+    .then(response => response.json())
+    .then(data => {      
+        console.log("Fetching latest image from server - data", data);
+        console.log("data.images.length", data.images.length);
+        // Loop through images and display 
+        for(let i = 0; i < data.images.length; i++){ 
+           let anImg = data.images[i];
+           appendImage(anImg);
+        }
+        // Set maxTimestamp
+        if (data.timestamp !== 0) {
+            maxTimestamp = data.timestamp;
+        }
+        
+        // Poll for new images every 5 seconds
+        setTimeout(fetchImages, 5000);
+    })
+    .catch(error => {
+        console.log("An error has occurred when attempting to Fetch:", error)
+    })
 
 }
 
