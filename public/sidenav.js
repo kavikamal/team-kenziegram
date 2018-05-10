0d3cb08a90e4974439c17ebcc528531e40bcbdf2
AJ Shannon <anjoshannon@gmail.com>
2018-05-10 10:59:43 -0400

    m

--- public/sidenav.js
+++ public/sidenav.js
@@ -3,4 +3,4 @@ document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.sidenav');
     const options ={};
     let instances = M.Sidenav.init(elems, options);
-})
+})  
