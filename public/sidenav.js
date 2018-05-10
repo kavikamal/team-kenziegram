4fd4c8ce377ea6778bbc0b3755e5e6cc84be6c72
AJ Shannon <anjoshannon@gmail.com>
2018-05-10 11:18:42 -0400

    m

--- public/sidenav.js
+++ public/sidenav.js
@@ -3,4 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.sidenav');
     const options ={};
     let instances = M.Sidenav.init(elems, options);
-})  
+})
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.materialboxed');
+    const options ={};
+    let instances = M.Materialbox.init(elems, options);
+  });
+
+//parallax 
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.parallax');
+    const options ={};
+    let instances = M.Parallax.init(elems, options);
+  });
+
