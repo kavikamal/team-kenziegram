4a11e53ea965ebdf2352d77919fe00846ee4c756
Christine Ooi <christine.ooi@gmail.com>
2018-05-11 11:47:13 -0400

    Removed unnecessary console.log statements and commented out the call to fetchImages() to stop the polling

--- index.js
+++ index.js
@@ -112,26 +112,20 @@ app.post('/upload', upload.single('myFile'), function (req, res, next) {
         .write(`${path}/${req.file.filename}`, function (err) {
             // This creates the new file with our modifications
             if (!err) console.log('Image Resized!')
-            
-                // This deletes the original file
-                console.log('Orginal file was Deleted')
-
                 res.render('indexpost.pug', { title: 'KenzieGram', imagename: `resized${req.file.filename}` });
             })
-            console.log(req.file.filename)
+            
         })
-    // items.push(req.file.filename);
 
 app.post('/createProfile', profilePicUpload.single('profilePic'), function (req, res) {
     // The GraphicsMagick module creates a thumbnail image from the uploaded profile picture
-    console.log(req.body.profilePic)
     gm(`${profilePicPath}/${req.body.profilePic}`)
         .resize(25, 25, '!')
         .noProfile()
         .compress('JPEG')
         .quality(85)
         .write(`${profilePicPath}/${req.body.profilePic}`, function (err) {
-            if (!err) console.log('Image Resized!')
+            if (!err) console.log('Profile Pic Resized!')
             console.log(err)
             const instance = new User({
                 name: req.body.name,
@@ -139,10 +133,6 @@ app.post('/createProfile', profilePicUpload.single('profilePic'), function (req,
                 messages: [],
                 posts: []
             });
-
-            console.log(req.body);
-            console.log("req.body.profilepic: ", req.body.profilePic);
-
 
             instance.save()
                 .then(instance => res.send())
