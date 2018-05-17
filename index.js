5799c51fa78a54b13100d704dce8421711f9dc3a
Kavitha Kamal <kavithakamal@gmail.com>
2018-05-17 13:36:33 -0400

    Display posts updated code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const multer = require('multer');
 const multerS3 = require('multer-s3')
@@ -7,28 +6,34 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-const AWS=require("aws-sdk");
-const gm = require('gm').subClass({ imageMagick: true });
-//let userName;
+const AWS = require("aws-sdk");
+const gm = require('gm').subClass({
+  imageMagick: true
+});
+
 
 //User can upload image types - (jpg|jpeg|png|gif)
 var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'public/uploads/') 
-    },
-    filename: (req, file, cb) => {
-      if (!file.originalname.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
-        return cb(new Error('Only image files are allowed!'), false);
-      }
-      cb(null,Date.now()  + '-' + file.originalname)
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads/')
+  },
+  filename: (req, file, cb) => {
+    if (!file.originalname.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
+      return cb(new Error('Only image files are allowed!'), false);
     }
-  });
-const upload = multer({storage: storage});
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+});
+const upload = multer({
+  storage: storage
+});
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.set('views', './views');
 app.set('view engine', 'pug');
 
@@ -49,76 +54,77 @@ db.on('error', console.error.bind(console, 'connection error: '));
 // Define Schemas 
 const Schema = mongoose.Schema;
 let userSchema = new Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-     
-    profilePic: String,
-    messages: [{
-        username: String,
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 
-        message: String,
-        timestamp: Number,
-    }],
-    posts: [{
-        image: String,
-        timestamp: Number,
-        // user: String,
-        caption: String,
-        comments: Array,
-    }]
-   
+  profilePic: String,
+  messages: [{
+    username: String,
+
+    message: String,
+    timestamp: Number,
+  }],
+  posts: [{
+    image: String,
+    timestamp: Number,
+    caption: String,
+    comments: Array,
+  }]
+
 });
 
 //authenticate input against database
 userSchema.statics.authenticate = function (username, password, callback) {
-    User.findOne({ username: username })
-      .exec(function (err, user) {
-        if (err) {
-          return callback(err)
-        } else if (!user) {
-          var err = new Error('User not found.');
-          err.status = 401;
-          return callback(err);
+  User.findOne({
+      username: username
+    })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
         }
-        bcrypt.compare(password, user.password, function (err, result) {
-          if (result === true) {
-            return callback(null, user);
-          } else {
-            return callback();
-          }
-        })
-      });
-  }
+      })
+    });
+}
 //hashing a password before saving it to the database
 userSchema.pre('save', function (next) {
-    var user = this;
-    bcrypt.hash(user.password, 10, function (err, hash){
-      if (err) {
-        return next(err);
-      }
-      user.password = hash;
-      next();
-    })
-  });
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
 
 
 //use sessions for tracking logins
 app.use(session({
-    secret: 'work hard',
-    resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({
-      mongooseConnection: db
-    })
-  }));
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
 
 // Compile User and Feed models from the schemas
 var User = mongoose.model('User', userSchema);
@@ -129,156 +135,177 @@ const IAM_USER_KEY = 'AKIAJ3VRLXCXWCSYLSIQ';
 const IAM_USER_SECRET = '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';
 
 var s3 = new AWS.S3({
-        accessKeyId: IAM_USER_KEY,
-        secretAccessKey: IAM_USER_SECRET,
-        Bucket: BUCKET_NAME
-    })
+  accessKeyId: IAM_USER_KEY,
+  secretAccessKey: IAM_USER_SECRET,
+  Bucket: BUCKET_NAME
+})
 // Adding the uploaded photos to our Amazon S3  bucket
 var imageUpload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'kenziegram',
-        metadata: function (req,file, cb) {
-            cb(null, Object.assign({}, req.body))
-        }
-    })
+  storage: multerS3({
+    s3: s3,
+    bucket: 'kenziegram',
+    metadata: function (req, file, cb) {
+      cb(null, Object.assign({}, req.body))
+    }
+  })
 });
 
 // Renders the main page along with all the images
-app.get('/', function (req, res) {  
-    fs.readdir(path, function(err, items) {   
-        res.render('signup',{title: 'KenzieGram'});
+app.get('/', function (req, res) {
+  fs.readdir(path, function (err, items) {
+    res.render('signup', {
+      title: 'KenzieGram'
     });
+  });
 })
 
 app.get('/register', (req, res) => {
-    res.render('signup', { title: 'Sign-up', success: req.session.success, errors: req.session.errors});
-    req.session.errors = null;
+  res.render('signup', {
+    title: 'Sign-up',
+    success: req.session.success,
+    errors: req.session.errors
+  });
+  req.session.errors = null;
 })
 
 app.get('/chat', (req, res) => {
-    res.render('chat')
+  res.render('chat')
 })
 
 app.get('/photos', (req, res) => {
-    User.findOne({ username: req.session.userId })
+  User.findOne({
+      username: req.session.userId
+    })
     .exec(function (err, user) {
       if (err) {
         return callback(err)
       } else if (!user) {
         var err = new Error('User not found.');
         err.status = 401;
-        res.render('error', { title: 'KenzieGram', errorMessage: err})
-      }else {
-         res.render('photos', { title: 'KenzieGram', posts: user.posts});
+        res.render('error', {
+          title: 'KenzieGram',
+          errorMessage: err
+        })
+      } else {
+        res.render('photos', {
+          title: 'KenzieGram',
+          posts: user.posts
+        });
       }
-      })
-    });
+    })
+});
 
 app.get('/post', (req, res) => {
-        res.render('post')
-    });
+  res.render('post')
+});
 // GET /logout
-app.get('/logout', function(req, res, next) {
-   
-    if (req.session) {
-      // delete session object
-      req.session.destroy(function(err) {
-        if(err) {
-          return next(err);
+app.get('/logout', function (req, res, next) {
 
-        } else {
-          return res.redirect('/');
-        }
-      });
-    }
-  });
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
 // Gets the latest images uploaded after a client-specified timestamp
 app.post('/latest', function (req, res, next) {
-    const latestImages = [];
-    const after = req.body.after;
+  const latestImages = [];
+  const after = req.body.after;
 
-    fs.readdir(path, function (err, items) {
-        // Loops through array of images to check if the modified time of each image
-        // is greater than the client specified timestamp that was passed in
-        // If so, store the new image(s) to be passed back along with the latest timestamp of all images
-        for (let i = 0; i < items.length; i++) {
-            let modified = fs.statSync(path + '/' + items[i]).mtimeMs;
-            if (modified > after) {
-                latestImages.push(items[i]);
-                maxTimestamp = modified > maxTimestamp ? modified : maxTimestamp;
-            }
-        }
-        res.send({ images: latestImages, timestamp: maxTimestamp });
+  fs.readdir(path, function (err, items) {
+    // Loops through array of images to check if the modified time of each image
+    // is greater than the client specified timestamp that was passed in
+    // If so, store the new image(s) to be passed back along with the latest timestamp of all images
+    for (let i = 0; i < items.length; i++) {
+      let modified = fs.statSync(path + '/' + items[i]).mtimeMs;
+      if (modified > after) {
+        latestImages.push(items[i]);
+        maxTimestamp = modified > maxTimestamp ? modified : maxTimestamp;
+      }
+    }
+    res.send({
+      images: latestImages,
+      timestamp: maxTimestamp
     });
+  });
 
 })
-
 
 // Uploads a new images and renders the uploaded page with the new image
 app.post('/upload', imageUpload.single('myFile'), function (req, res, next) {
-    
-    console.log(req.session.userId);
-    //res.render('photos.pug', { title: 'KenzieGram', imagename: `${req.file.filename}` });
-    
-    let post = {
-        image: req.file.location,
-        timestamp: Date.now,
-        // user: req.body.name,
-        caption: req.body.caption,
-        comments: [],
-    };
-    db.collection('users').findOneAndUpdate({ "username": req.session.userId }, {$push: {"posts": post} })   
-    return res.redirect('/photos');  
+  let post = {
+    image: req.file.location,
+    timestamp: Date.now,
+    caption: req.body.caption,
+    comments: [],
+  };
+  db.collection('users').findOneAndUpdate({
+    "username": req.session.userId
+  }, {
+    $push: {
+      "posts": post
+    }
+  })
+  return res.redirect('/photos');
 })
-      
+
 app.post('/createProfile', imageUpload.single('profilePic'), function (req, res, next) {
+  const instance = new User({
+    username: req.body.name,
+    password: req.body.password,
+    profilePic: req.file.location,
+    messages: [],
+    posts: []
+  });
+  instance.save()
+    .then(instance => res.send())
+  res.render('photos.pug', {
+    title: 'KenzieGram',
+    arrayofimages: items,
+    userName: req.body.name
+  });
 
-            const instance = new User({
-                username: req.body.name,
-                password: req.body.password,
-                passwordConf: req.body.confirmPassword,
-                profilePic: req.file.location, 
-                messages: [],
-                posts: []
-            });
-            instance.save()
-                .then(instance => res.send())
-            res.render('photos.pug', { title: 'KenzieGram', arrayofimages: items, userName: req.body.name });
-
-        });
-    //}
-// });
+});
 
 // Endpoint for login instead of creating a new profile
 app.post('/login', (req, res) => {
-    //userName = req.body.name;
-    if (req.body.name && req.body.password) {
-        User.authenticate(req.body.name, req.body.password, function (error, user) {
-          if (error || !user) {
-            var err = new Error('Wrong Username or password.');
-            err.status = 401;
-            console.log(err);
-            res.render('error', { title: 'KenzieGram', errorMessage: err});
-          } else {
-            req.session.userId = user.username;
-            console.log("helloooooo---",session.username, session.userId);
-            console.log(user.posts);
-            res.render('photos', { title: 'KenzieGram', posts: user.posts})
-          }
+  if (req.body.name && req.body.password) {
+    User.authenticate(req.body.name, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong Username or password.');
+        err.status = 401;
+        console.log(err);
+        res.render('error', {
+          title: 'KenzieGram',
+          errorMessage: err
         });
       } else {
-        var err = new Error('All fields required.');
-        err.status = 400;
-        console.log(err)
-        res.render('error', { title: 'KenzieGram', errorMessage: err})
+        req.session.userId = user.username;
+        res.render('photos', {
+          title: 'KenzieGram',
+          posts: user.posts
+        })
       }
-  
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    console.log(err)
+    res.render('error', {
+      title: 'KenzieGram',
+      errorMessage: err
+    })
+  }
+
 })
 
 app.listen(PORT, () => {
-    mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_URI}/${dbName}`);
-    // mongoose.connect('mongodb://localhost/xforcekenziegram')
-    console.log(`listening at port ${PORT}`);
+  mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_URI}/${dbName}`);
+  console.log(`listening at port ${PORT}`);
 })
-
